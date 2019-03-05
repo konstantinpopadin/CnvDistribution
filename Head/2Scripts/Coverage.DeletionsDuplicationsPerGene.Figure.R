7bad3f9e75588a9f0176d663b620c4fb959ec374
konstantinpopadin <konstantinpopadin@gmail.com>
2019-03-05 15:46:24 +0100

    deletions and duplications

--- Head/2Scripts/Coverage.DeletionsDuplicationsPerGene.Figure.R
+++ Head/2Scripts/Coverage.DeletionsDuplicationsPerGene.Figure.R
@@ -4,17 +4,30 @@ Genes = read.table("../../Body/3Results/Coverage.DeletionsDuplicationsPerGene.Ta
 
 pdf("../../Body/4Figures/Coverage.DeletionsDuplicationsPerGene.Figure.R01.pdf")
 Genes = Genes[!is.na(Genes$DuplCov) & !is.na(Genes$DelCov),]
+Genes = Genes[Genes$DuplCov > 0 & Genes$DelCov > 0,]  # at least one is higher than zero
 
 XMin = -10 # log2(0.001)
 XMax = max(log2(Genes$DelCov))
 YMin = -10
 YMax = max(log2(Genes$DuplCov))
 
-par(mfrow=c(2,2))
-plot(log2(Genes$DelCov),log2(Genes$DuplCov), pch = 16, xlim = c(XMin,XMax), ylim = c(YMin,YMax), col = rgb(0.1,0.1,0.1,0.1))
+par(mfrow=c(2,3))
+plot(log2(Genes$DelCov),log2(Genes$DuplCov), pch = 16, xlim = c(XMin,XMax), ylim = c(YMin,YMax), col = rgb(0.1,0.1,0.1,0.1), xlab = 'deletions', ylab = 'duplications', main = 'all genes')
+#a <- lm(log2(Genes$DuplCov) ~ log2(Genes$DelCov))
+#abline(a, col = 'grey')
 cor.test(log2(Genes$DelCov),log2(Genes$DuplCov), method = 'spearman')
-# par(new=TRUE)
-plot(log2(Genes[Genes$GeneType == 'protein_coding',]$DelCov),log2(Genes[Genes$GeneType == 'protein_coding',]$DuplCov),  pch = 16, xlim = c(XMin,XMax), ylim = c(YMin,YMax), col = rgb(0.5,0.1,0.1,0.1))
+summary(log2(Genes$DelCov))
+
+plot(log2(Genes[Genes$GeneType == 'protein_coding',]$DelCov),log2(Genes[Genes$GeneType == 'protein_coding',]$DuplCov),  pch = 16, xlim = c(XMin,XMax), ylim = c(YMin,YMax), col = rgb(0.5,0.1,0.1,0.1), xlab = 'deletions', ylab = 'duplications', main = 'protein-coding genes')
+#a <- lm(log2(Genes[Genes$GeneType == 'protein_coding',]$DelCov) ~ log2(Genes[Genes$GeneType == 'protein_coding',]$DuplCov))
+#abline(a, col = 'red')
 cor.test(log2(Genes[Genes$GeneType == 'protein_coding',]$DelCov),log2(Genes[Genes$GeneType == 'protein_coding',]$DuplCov), method = 'spearman')
 
+GWHS = Genes[!is.na(Genes$GenomeWideHaploinsufficiencyScore) & Genes$GenomeWideHaploinsufficiencyScore > 0,]
+plot(log2(GWHS[GWHS$GenomeWideHaploinsufficiencyScore > median(GWHS$GenomeWideHaploinsufficiencyScore),]$DelCov),log2(GWHS[GWHS$GenomeWideHaploinsufficiencyScore > median(GWHS$GenomeWideHaploinsufficiencyScore),]$DuplCov),  pch = 16, xlim = c(XMin,XMax), ylim = c(YMin,YMax), col = rgb(0.1,0.3,0.1,0.1), xlab = 'deletions', ylab = 'duplications', main = 'haploinsufficient genes')
+#a <- lm(log2(GWHS[GWHS$GenomeWideHaploinsufficiencyScore > median(GWHS$GenomeWideHaploinsufficiencyScore),]$DelCov) ~ log2(GWHS[GWHS$GenomeWideHaploinsufficiencyScore > median(GWHS$GenomeWideHaploinsufficiencyScore),]$DuplCov))
+#abline(a, col = 'green')
+cor.test(log2(GWHS[GWHS$GenomeWideHaploinsufficiencyScore > median(GWHS$GenomeWideHaploinsufficiencyScore),]$DelCov),log2(GWHS[GWHS$GenomeWideHaploinsufficiencyScore > median(GWHS$GenomeWideHaploinsufficiencyScore),]$DuplCov), method = 'spearman')
+
 dev.off()
+
---
+++ Head/2Scripts/Coverage.DeletionsDuplicationsPerGene.SelectionComponent.R
@@ -0,0 +1,48 @@
+rm(list=ls(all=TRUE))
+
+Genes = read.table("../../Body/3Results/Coverage.DeletionsDuplicationsPerGene.Table.txt", header = TRUE)
+
+pdf("../../Body/4Figures/Coverage.DeletionsDuplicationsPerGene.SelectionComponent.R01.pdf")
+
+# if there are no DupCov or DelCov - it means they are zero covered...
+####### 1: deletions are more deleterious than duplications
+
+VecOfParameters = c('KnKsMouse','GenomeWideHaploinsufficiencyScore','FunctionalIndispensabilityScore','ProbOfBeingLofIntolerant','ResidualVariationIntoleranceScore')
+for (param in 1:length(VecOfParameters))
+{  # param = 1
+if (param == 1) {TEMP = data.frame(Genes$KnKsMouse,Genes$DelCov,Genes$DuplCov)}
+if (param == 2) {TEMP = data.frame(Genes$GenomeWideHaploinsufficiencyScore,Genes$DelCov,Genes$DuplCov)}
+if (param == 3) {TEMP = data.frame(Genes$FunctionalIndispensabilityScore,Genes$DelCov,Genes$DuplCov)}  
+if (param == 4) {TEMP = data.frame(Genes$ProbOfBeingLofIntolerant,Genes$DelCov,Genes$DuplCov)}    
+if (param == 5) {TEMP = data.frame(Genes$ResidualVariationIntoleranceScore,Genes$DelCov,Genes$DuplCov)}    
+  
+TEMP[,2:3][is.na(TEMP[,2:3] )] = 0 
+
+DelVecRho=c(); DelVecP=c(); DuplVecRho=c(); DuplVecP=c()
+for (i in 1:500)
+  {
+  temp = TEMP[sample(nrow(TEMP),5000),]
+  
+  Rho = as.numeric(cor.test(temp[,1],temp[,2], method = 'spearman')[4])
+  P = as.numeric(cor.test(temp[,1],temp[,2], method = 'spearman')[3])
+  DelVecRho = c(DelVecRho,Rho); DelVecP = c(DelVecP,P)
+  
+  Rho = as.numeric(cor.test(temp[,1],temp[,3], method = 'spearman')[4])
+  P = as.numeric(cor.test(temp[,1],temp[,3], method = 'spearman')[3])
+  DuplVecRho = c(DuplVecRho,Rho); DuplVecP = c(DuplVecP,P)
+  }
+
+if (param == 1) {KnKsMouseDelVecRho = DelVecRho; KnKsMouseDuplVecRho = DuplVecRho; }
+if (param == 2) {GenomeWideHaploinsufficiencyScoreDelVecRho = DelVecRho; GenomeWideHaploinsufficiencyScoreDuplVecRho = DuplVecRho; }
+if (param == 3) {FunctionalIndispensabilityScoreDelVecRho = DelVecRho; FunctionalIndispensabilityScoreDuplVecRho = DuplVecRho; }
+if (param == 4) {ProbOfBeingLofIntolerantDelVecRho = DelVecRho; ProbOfBeingLofIntolerantDuplVecRho = DuplVecRho; }
+if (param == 5) {ResidualVariationIntoleranceScoreDelVecRho = DelVecRho; ResidualVariationIntoleranceScoreDuplVecRho = DuplVecRho; }
+}
+
+boxplot(KnKsMouseDelVecRho,KnKsMouseDuplVecRho,GenomeWideHaploinsufficiencyScoreDelVecRho,GenomeWideHaploinsufficiencyScoreDuplVecRho,FunctionalIndispensabilityScoreDelVecRho,FunctionalIndispensabilityScoreDuplVecRho,ProbOfBeingLofIntolerantDelVecRho,ProbOfBeingLofIntolerantDuplVecRho,ResidualVariationIntoleranceScoreDelVecRho,ResidualVariationIntoleranceScoreDuplVecRho, col = c('green','blue'), outline = FALSE, notch = TRUE)
+abline(h=0, col = 'red')
+
+####### 2: selection and frequency - doesn't work for now !!! ???
+
+dev.off()
+
